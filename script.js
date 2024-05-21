57d74a129ca02e59ba36aac8293c3156c7218331
Marcelo Lameu <cellinho35@gmail.com>
2024-05-21 16:37:24 -0300

    Alteraçoes no html de cadastro e no js geral

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Validação de formulário de cadastro
+// Validação de formulário de cadastro local
 function validateCadastroForm() {
     const nome = document.getElementById('nome-cadastro').value;
     const sobrenome = document.getElementById('sobrenome-cadastro').value;
@@ -20,9 +20,21 @@ function validateCadastroForm() {
         return false;
     }
 
-    // Se a validação passar, o formulário é enviado
-    alert('Cadastro realizado com sucesso!');
+    // Salva as informações de cadastro localmente
+    saveCadastroInfo(nome, sobrenome, cpf, email, telefone, senha);
+
+    // Exibe o popup de cadastro realizado com sucesso
+    showCadastroPopup();
+
+    // Redireciona para a página de quadras após o cadastro com sucesso
+    navigateTo('quadras');
+
     return true;
+}
+
+// Função para exibir popup de cadastro realizado com sucesso
+function showCadastroPopup() {
+    alert('Cadastro realizado com sucesso!');
 }
 
 // Adiciona eventos de clique aos botões
@@ -37,15 +49,23 @@ document.addEventListener('DOMContentLoaded', function () {
     const cadastrarBtn = document.getElementById('cadastrar-btn');
     if (cadastrarBtn) {
         cadastrarBtn.addEventListener('click', function () {
-            navigateTo('cadastro');
+            validateCadastroForm();
         });
     }
 
     const cadastrarGoogleBtn = document.getElementById('btn-google-cadastro');
     if (cadastrarGoogleBtn) {
         cadastrarGoogleBtn.addEventListener('click', function () {
-            // Aqui você pode adicionar a lógica para cadastro com conta do Google
-            alert('Cadastro com Google em breve disponível.');
+            // Chama a função de login do Google
+            auth2.signIn().then(function(googleUser) {
+                console.log('Usuário autenticado com sucesso:', googleUser);
+                // Após o login com sucesso, salva o status no Local Storage
+                localStorage.setItem('googleUser', 'true');
+                // Redireciona para a página de quadras após o login com sucesso
+                navigateTo('quadras');
+            }, function(error) {
+                console.error('Erro ao autenticar usuário:', error);
+            });
         });
     }
 
@@ -56,4 +76,7 @@ document.addEventListener('DOMContentLoaded', function () {
             validateCadastroForm();
         });
     }
+
+    // Verifica o status do login do Google ao carregar a página
+    checkGoogleLoginStatus();
 });
